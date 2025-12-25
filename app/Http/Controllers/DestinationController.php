02812d63702e67fb9f8610423278d80f14609ecd
Ashmoon32 <nyeinmaung3021@gmail.com>
2025-12-25 22:27:57 +0630

    Add Validation

--- app/Http/Controllers/DestinationController.php
+++ app/Http/Controllers/DestinationController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Destination;
 
 
 class DestinationController extends Controller
@@ -12,5 +13,23 @@ class DestinationController extends Controller
         $places = \App\Models\Destination::all();
 
         return response()->json($places);
+    }
+
+    public function store(Request $request)
+    {
+        // Rules for validation
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'required|min:10',
+        ]);
+
+        // Action
+        $destination = Destination::create($validatedData);
+
+        // Response
+        return response()->json([
+            'message' => 'Destination added to your bucket list!',
+            'data' => $destination
+        ], 201); // successful created
     }
 }
